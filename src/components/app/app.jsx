3309ede24c1534c8f7c2f97b5c6727d231b8ec0b
artytrik <tema-luch@mail.ru>
2019-10-18 22:58:41 +0300

    Добавил props и prop-types

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {Main} from '../main/main.jsx';
+import {GENRES_LIST} from '../../utils.js';
 
-export const App = () => {
-  return <Main />;
+export const App = (props) => {
+  const {moviesList} = props;
+
+  return <Main movies = {moviesList}/>;
+};
+App.propTypes = {
+  moviesList: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    genre: PropTypes.oneOf(GENRES_LIST).isRequired
+  }))
 };
---
+++ src/utils.js
@@ -0,0 +1,2 @@
+export const GENRES_LIST = [`Comedy`, `Crime`, `Documentary`, `Drama`,
+  `Horror`, `Kids & Family`, `Romance`, `Sci-Fi`, `Thriller`];
