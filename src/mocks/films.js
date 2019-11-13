5e337175c9c65ab93316907aa51b81f19c851867
artytrik <tema-luch@mail.ru>
2019-11-13 21:06:49 +0300

    Добавил в моки id и ссылки на превью

--- src/mocks/films.js
+++ src/mocks/films.js
@@ -1,27 +1,37 @@
 export const moviesListData = [
   {
+    id: 1,
     name: `Fantastic Beasts: The Crimes of Grindelwald`,
     genre: `Sci-Fi`,
-    preview: `/img/fantastic-beasts-the-crimes-of-grindelwald.jpg`
+    preview: `/img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    link: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`
   },
   {
+    id: 2,
     name: `Bohemian Rhapsody`,
     genre: `Documentary`,
-    preview: `img/bohemian-rhapsody.jpg`
+    preview: `img/bohemian-rhapsody.jpg`,
+    link: https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm
   },
   {
+    id: 3,
     name: `Macbeth`,
     genre: `Romance`,
-    preview: `img/macbeth.jpg`
+    preview: `img/macbeth.jpg`,
+    link: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`
   },
   {
+    id: 4,
     name: `Aviator`,
     genre: `Drama`,
-    preview: `img/aviator.jpg`
+    preview: `img/aviator.jpg`,
+    link: https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm
   },
   {
+    id: 5,
     name: `Pulp Fiction`,
     genre: `Crime`,
-    preview: `img/pulp-fiction.jpg`
+    preview: `img/pulp-fiction.jpg`,
+    link: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`
   }
 ];
